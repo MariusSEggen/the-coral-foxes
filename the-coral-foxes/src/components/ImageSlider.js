2c325eaac7a05e8e92239f69db1c2ce15fed0d04
Andreas Winther Moen <andreas@winthermoen.no>
2022-02-16 14:27:56 +0100

    Add image slider component

---
+++ the-coral-foxes/src/components/ImageSlider.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import Slider from "@mui/material/Slider";
+
+const ImageSlider = ({ oldPic, newPic }) => {
+  const [ratio, setRatio] = useState(80);
+  const [mousePos, setMousePos] = useState();
+
+  const onHover = (ev) => {
+    const currentTargetRect = ev.currentTarget.getBoundingClientRect();
+    const event_offset = ev.pageX - currentTargetRect.left;
+    const newRatio = ((event_offset / currentTargetRect.width) * 100).toFixed(
+      0
+    );
+    setRatio(newRatio);
+  };
+
+  return (
+    <div>
+      <img
+        onMouseEnter={() => console.log("on mouse enter")}
+        onMouseLeave={() => console.log("on mouse leave")}
+        onMouseMove={onHover}
+        style={{
+          position: "absolute",
+          left: "10%",
+          top: "5%",
+          width: "80%",
+          height: "50%",
+        }}
+        src={process.env.PUBLIC_URL + oldPic}
+        alt="old"
+      />
+      <img
+        style={{
+          position: "absolute",
+          left: "10%",
+          top: "5%",
+          width: "80%",
+          height: "50%",
+          objectFit: "cover",
+          clipPath: `polygon(0 0 , ${ratio}% 0, ${ratio}% 100%, 0 100%)`,
+          pointerEvents: "none",
+        }}
+        src={process.env.PUBLIC_URL + oldPic}
+        alt="old"
+      />
+      <img
+        style={{
+          position: "absolute",
+          left: "10%",
+          top: "5%",
+          width: "80%",
+          height: "50%",
+          objectFit: "cover",
+          clipPath: `polygon(${ratio}% 0 , 100% 0, 100% 100%, ${ratio}% 100%)`,
+          pointerEvents: "none",
+        }}
+        src={process.env.PUBLIC_URL + newPic}
+        alt="new"
+      />
+      <div
+        style={{
+          backgroundColor: "blue",
+          position: "absolute",
+          width: "2px",
+          height: "50%",
+          top: "5%",
+          pointerEvents: "none",
+          left:
+            (mousePos && mousePos - 1) || `calc(${10 + ratio * 0.8}% - 1px)`,
+        }}
+      ></div>
+      <div
+        style={{
+          width: "80%",
+          position: "relative",
+          left: "10%",
+          top: "450px",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default ImageSlider;
